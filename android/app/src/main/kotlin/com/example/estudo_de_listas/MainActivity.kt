c8e3a0826216225757796df7df2a78f0c9197d20
Henrique Moles <32689505+henriquemoles@users.noreply.github.com>
2021-03-06 17:55:54 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/estudo_de_listas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estudo_de_listas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:estudo_de_listas/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+));
